3f20e5ccc6b4d74d2c68853cc5c2233e777d655e
huynvn <Huy.Nguyen@navigosgroup.com>
2023-01-29 10:38:23 +0700

    152: Cau hinh React router

---
+++ ShopeeClone/src/useRouteElement.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useRoutes } from 'react-router-dom'
+import RegisterLayout from './layout/RegisterLayout'
+import Login from './pages/Login'
+import ProductList from './pages/ProductList'
+import Register from './pages/Register'
+
+export default function useRouteElement() {
+  const routeElement = useRoutes([
+    { path: '/', element: <ProductList /> },
+    {
+      path: '/login',
+      element: (
+        <RegisterLayout>
+          <Login />
+        </RegisterLayout>
+      )
+    },
+    {
+      path: '/register',
+      element: (
+        <RegisterLayout>
+          <Register />
+        </RegisterLayout>
+      )
+    }
+  ])
+  return routeElement
+}
