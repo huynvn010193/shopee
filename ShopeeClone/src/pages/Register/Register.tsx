aae33fe9514249a722d7a8a4d3843213b3ed693b
huynvn <Huy.Nguyen@navigosgroup.com>
2023-02-12 17:15:43 +0700

    156.Validate Confirm Password cho Register Form

--- ShopeeClone/src/pages/Register/Register.tsx
+++ ShopeeClone/src/pages/Register/Register.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
-import { rules } from 'src/utils/rules'
+import { getRules } from 'src/utils/rules'
 
 interface FormData {
   email: string
@@ -13,14 +13,23 @@ export default function Register() {
   const {
     register,
     handleSubmit,
+    watch,
+    getValues,
     formState: { errors }
   } = useForm<FormData>()
 
-  const onSubmit = handleSubmit((data) => {
-    console.log(data)
-  })
+  const onSubmit = handleSubmit(
+    (data) => {
+      console.log(data)
+    },
+    (data) => {
+      // khi submit lỗi sẽ chạy xuống đây
+      const password = getValues('password')
+      console.log('password', password)
+    }
+  )
 
-  console.log('errors', errors)
+  const rules = getRules(getValues)
 
   return (
     <div className='bg-orange'>
@@ -43,6 +52,7 @@ export default function Register() {
                   type='password'
                   className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
                   placeholder='Password'
+                  autoComplete='on'
                   {...register('password', rules.password)}
                 />
                 <div className='mt-1 min-h-[1.25rem] text-sm text-red-600'>{errors.password?.message}</div>
@@ -52,6 +62,7 @@ export default function Register() {
                   type='password'
                   className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
                   placeholder='Confirm Password'
+                  autoComplete='on'
                   {...register('confirm_password', rules.confirm_password)}
                 />
                 <div className='mt-1 min-h-[1.25rem] text-sm text-red-600'>{errors.confirm_password?.message}</div>
