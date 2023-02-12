4a199c59b966f3bc99b92162c8e73532ea6256e9
huynvn <Huy.Nguyen@navigosgroup.com>
2023-02-12 21:12:30 +0700

    158.Custom class container bằng cách tạo plugin Tailwindcss

--- ShopeeClone/src/components/RegisterHeader/RegisterHeader.tsx
+++ ShopeeClone/src/components/RegisterHeader/RegisterHeader.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function RegisterHeader() {
   return (
     <header className='py-5'>
-      <div className='mx-auto max-w-7xl px-4'>
+      <div className='container'>
         <nav className='items- flex'>
           <Link to='/'>
             <svg viewBox='0 0 192 65' className='h-8 fill-orange lg:h-11'>
