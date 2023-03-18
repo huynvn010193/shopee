680a622bad5e7dc45ba3e9eeb6196670f369e4a0
huynvn <Huy.Nguyen@navigosgroup.com>
2023-03-18 08:36:11 +0700

    164.Xử lý lỗi message đơn giản với axios Interception

--- ShopeeClone/src/App.tsx
+++ ShopeeClone/src/App.tsx
@@ -1,8 +1,15 @@
 import useRouteElement from './useRouteElement'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   const routeElements = useRouteElement()
-  return <div>{routeElements}</div>
+  return (
+    <div>
+      {routeElements}
+      <ToastContainer />
+    </div>
+  )
 }
 
 export default App
